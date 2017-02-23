ad1213beae676e8fe2eb6bc760b9eac1f8824791
tomhuhges <merzbear@gmail.com>
2017-02-23 17:20:25 +0000

    tidy up

--- src/editor/component.jsx
+++ src/editor/component.jsx
@@ -31,10 +31,7 @@ class Editor extends React.Component {
   componentWillReceiveProps(nextProps) {
     if (nextProps.currentFile) {
       dropbox.getFileContent(nextProps.currentFile, this.props.accessToken)
-        .then(response => this.setState({
-          value: response,
-          isLoading: false,
-        }, browserHistory.push(`/edit${this.props.currentFile}`)))
+        .then(fileContent => this.setState({ value: fileContent, isLoading: false }))
         .catch(err => console.error(err.error))
     }
   }
@@ -50,7 +47,10 @@ class Editor extends React.Component {
           {this.state.isLoading ? (
             <div className="w-100 h-100 flex justify-center items-center">
               <p>Setting up your workspace...</p>
-              <img src="http://25.media.tumblr.com/4abad145cfeca409f3f76cac7e9393de/tumblr_mq50y4zfz71szhoyto1_400.gif" alt="" />
+              <img
+                src="http://25.media.tumblr.com/4abad145cfeca409f3f76cac7e9393de/tumblr_mq50y4zfz71szhoyto1_400.gif"
+                alt=""
+              />
             </div>
           ) : (
             <ReactCodeMirror
