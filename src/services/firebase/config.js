4cb4a25d055d96d5faec7bffd16f640e7fb19108
tomhuhges <merzbear@gmail.com>
2017-06-09 01:40:04 +0100

    deploy

---
+++ src/services/firebase/config.js
@@ -0,0 +1,8 @@
+const config = {
+  apiKey: 'AIzaSyCtd6CkhMpN6ZKm7V5KCrCCFf3GympJriI',
+  authDomain: 'kesey-e1f4d.firebaseapp.com',
+  databaseURL: 'https://kesey-e1f4d.firebaseio.com/',
+  storageBucket: 'kesey-e1f4d.appspot.com',
+};
+
+export default config;
