ad541c9eb3bd738181c3f353c1f9933e51848008
tomhuhges <merzbear@gmail.com>
2017-06-09 02:49:52 +0100

    deploy

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -25,7 +25,7 @@ module.exports = {
   module: {
     loaders: [{
       test: /\.jsx?$/,
-      loaders: ['babel-loader'],
+      loaders: ['babel'],
       include: path.join(__dirname, 'src'),
     },
     {
